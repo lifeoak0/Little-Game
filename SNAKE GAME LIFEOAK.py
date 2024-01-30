65cff10ecf8a3e353ebd77dd1366148c4043ac68
JLiu <107297778+lifeoak0@users.noreply.github.com>
2024-01-30 19:13:07 +0800

    Update SNAKE GAME LIFEOAK.py

--- SNAKE GAME LIFEOAK.py
+++ SNAKE GAME LIFEOAK.py
@@ -3,22 +3,18 @@ import time
 import random
 
 pygame.init()
-
 white = (255, 255, 255)
 yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
-
 dis_width = 600
 dis_height = 400
-
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by LIKEOAK')
 
 clock = pygame.time.Clock()
-
 snake_block = 10
 snake_speed = 15
 
